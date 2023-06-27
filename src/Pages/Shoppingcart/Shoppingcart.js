98717c0e7338df122ce7b9d86da1ce2d0d5d79ab
RosalBez <rosalbezuijen@hotmail.com>
2023-06-27 10:15:22 +0200

    homepage

---
+++ src/Pages/Shoppingcart/Shoppingcart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Shopingcart(props) {
+    return (
+        <div>
+            <h2> jouw winkelmandje</h2>
+            <p> lekker betalen</p>
+        </div>
+    );
+}
+
+export default Shopingcart;
